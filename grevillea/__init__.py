abad2e36511519a24b6e4cdf27e224d163774ab3
erm <jordan@eremieff.com>
2019-11-11 14:34:26 +1100

    Work out GCF specific response handling, types, fix issues with body

--- grevillea/__init__.py
+++ grevillea/__init__.py
@@ -1,39 +1,66 @@
 import asyncio
 import enum
-from typing import Any
-from io import BytesIO
-from dataclasses import dataclass, field
+import typing
+from dataclasses import dataclass
+
+
+from grevillea.types import ASGIApp, Scope, Message
 
 
 class ASGICycleState(enum.Enum):
     REQUEST = enum.auto()
     RESPONSE = enum.auto()
+    COMPLETE = enum.auto()
 
 
 @dataclass
 class ASGICycle:
-    scope: dict
-    body: BytesIO = BytesIO()
+    scope: Scope
+    loop: asyncio.AbstractEventLoop
+    body: bytes = b""
+    status_code: int = 200
     state: ASGICycleState = ASGICycleState.REQUEST
-    response: dict = field(default_factory=dict)
 
-    def __call__(self, app: Any, body: bytes) -> dict:
-        loop = asyncio.new_event_loop()
-        self.app_queue: asyncio.Queue = asyncio.Queue(loop=loop)
-        self.put_message({"type": "http.request", "body": body, "more_body": False})
-        asgi_instance = app(self.scope, self.receive, self.send)
-        asgi_task = loop.create_task(asgi_instance)
-        loop.run_until_complete(asgi_task)
-        return self.response
+    def __post_init__(self) -> None:
+        self.app_queue: asyncio.Queue = asyncio.Queue(loop=self.loop)
 
-    def put_message(self, message: dict) -> None:
+    def __call__(self, app) -> str:
+        asgi_instance = self.run(app)
+        asgi_task = self.loop.create_task(asgi_instance)
+        self.loop.run_until_complete(asgi_task)
+        return self.body.decode(), self.status_code
+
+    def put_message(self, message: Message) -> None:
         self.app_queue.put_nowait(message)
 
-    async def receive(self) -> dict:
+    async def run(self, app: ASGIApp) -> None:
+        try:
+            await app(self.scope, self.receive, self.send)
+        except BaseException as exc:
+            msg = "Exception in ASGI application\n"
+            self.logger.error(msg, exc_info=exc)
+            if self.state is ASGICycleState.REQUEST:
+                await self.send(
+                    {
+                        "type": "http.response.start",
+                        "status": 500,
+                        "headers": [(b"content-type", b"text/plain; charset=utf-8")],
+                    }
+                )
+                await self.send(
+                    {"type": "http.response.body", "body": b"Internal Server Error"}
+                )
+                self.state = ASGICycleState.COMPLETE
+
+            elif self.state is not ASGICycleState.COMPLETE:
+                self.body = b"Internal Server Error"
+                self.status_code = 500
+
+    async def receive(self) -> Message:
         message = await self.app_queue.get()
         return message
 
-    async def send(self, message: dict) -> None:
+    async def send(self, message: Message) -> None:
         message_type = message["type"]
 
         if self.state is ASGICycleState.REQUEST:
@@ -41,14 +68,6 @@ class ASGICycle:
                 raise RuntimeError(
                     f"Expected 'http.response.start', received: {message_type}"
                 )
-
-            status_code = message["status"]
-            headers = {k: v for k, v in message.get("headers", [])}
-
-            self.response["status_code"] = status_code
-            self.response["headers"] = {
-                k.decode(): v.decode() for k, v in headers.items()
-            }
 
             self.state = ASGICycleState.RESPONSE
 
@@ -61,19 +80,19 @@ class ASGICycle:
             content = message.get("body", b"")
             more_body = message.get("more_body", False)
 
-            self.body.write(content)
+            self.body += content
 
             if not more_body:
-                self.response["body"] = self.body.getvalue().decode()
                 self.put_message({"type": "http.disconnect"})
+                self.state = ASGICycleState.COMPLETE
 
 
 @dataclass
 class Grevillea:
 
-    app: Any
+    app: ASGIApp
 
-    def __call__(self, request) -> str:
+    def __call__(self, request) -> typing.Tuple[str, int]:
         try:
             response = self.asgi(request)
         except BaseException as exc:
@@ -81,7 +100,10 @@ class Grevillea:
         else:
             return response
 
-    def asgi(self, request) -> str:
+    def asgi(self, request) -> typing.Tuple[str, int]:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
         environ = request.environ
         scope = {
             "type": "http",
@@ -91,10 +113,14 @@ class Grevillea:
             "path": request.path,
             "scheme": request.scheme,
             "http_version": "1.1",
-            "root_path": "",
+            "root_path": environ["RAW_URI"] or None,
             "query_string": request.query_string,
             "headers": [[k.encode(), v.encode()] for k, v in request.headers],
         }
         body = request.get_data() or b""
-        response = ASGICycle(scope)(self.app, body=body)
-        return response["body"]
+        asgi_cycle = ASGICycle(scope, loop=loop)
+        asgi_cycle.put_message(
+            {"type": "http.request", "body": body, "more_body": False}
+        )
+        response = asgi_cycle(self.app)
+        return response
---
+++ grevillea/types.py
@@ -0,0 +1,7 @@
+import typing
+
+Scope = typing.Dict[str, typing.Any]
+Message = typing.Dict[str, typing.Any]
+Receive = typing.Callable[[], typing.Awaitable[Message]]
+Send = typing.Callable[[Message], typing.Awaitable[None]]
+ASGIApp = typing.Callable[[Scope, Receive, Send], typing.Awaitable[None]]
